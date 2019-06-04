1f335eee37653707cb65682513359bb0163f592c
shaminGit <shaminali14@gmail.com>
2019-06-04 16:47:38 +0530

    new animation with circle

--- canvas.js
+++ canvas.js
@@ -4,43 +4,52 @@ canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
 let c = canvas.getContext("2d"); // master object (x,y , width , height)
+c.strokeStyle = "rgba(255,0,0,0.7)";
+var radius = 100,
+  radius1 = 40;
 
-var radius = 40;
 let x = innerWidth / 2;
+let x1 = innerWidth / 2;
 let y = innerHeight / 2;
-let x1 = innerHeight / 2;
-let y1 = innerWidth / 2;
-console.log(x, y);
+let y1 = innerHeight / 2;
 let dx = 4;
+
+/* for (let i = 0; i < 10; i++) {
+  c.beginPath();
+  c.arc(x, y, radius, 0, Math.PI * 2, false); //
+  c.strokeStyle = "blue";
+  c.fillStyle = "yellow";
+  c.fill();
+  c.stroke();
+} */
 
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, innerWidth, innerHeight);
   c.beginPath();
   c.arc(x, y, radius, 0, Math.PI * 2, false); //
+  c.strokeStyle = "blue";
+  c.fillStyle = "purple";
+  c.fill();
+  c.stroke();
 
+  if (x + radius > innerWidth || y + radius > innerHeight) {
+    dx = -dx;
+  }
+  // x += dx;
+  radius += dx;
+
+  if (radius1 == 0) {
+    dx = -dx;
+  }
+  c.beginPath();
+  c.arc(x1, y1, radius1, 0, Math.PI * 2, false); //
   c.strokeStyle = "blue";
   c.fillStyle = "yellow";
   c.fill();
   c.stroke();
-
-  if (x + radius > innerWidth || x - radius < 0) {
-    dx = -dx;
-  }
-  x += dx;
-
-  c.beginPath();
-  c.arc(y1, x1, radius, 0, Math.PI * 2, false); //
-
-  c.strokeStyle = "blue";   //  
-  c.fillStyle = "yellow";    //
-  c.fill();
-  c.stroke();
-
-  if (x1 + radius > innerHeight || x1 - radius < 0) {
-    dx = -dx;
-  }
-  x1 += dx;
+  // x += dx;
+  radius1 += dx;
 }
 
 animate();
